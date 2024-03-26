b2e0c06727ffb91399c0f2a2d34945d7c45c6ec5
mike-menyala <159994059+mike-menyala@users.noreply.github.com>
2024-03-26 16:09:32 +0800

    commit message for multiple files

--- packages/hardhat/contracts/LoyaltyApp.sol
+++ packages/hardhat/contracts/LoyaltyApp.sol
@@ -7,44 +7,37 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LoyaltyApp is ERC721, Ownable {
-    // Token ID counter
     uint256 private tokenIdCounter;
-
-    // Mapping to keep track of token burn status
     mapping(uint256 => bool) private isTokenBurnt;
-
-    // Flag to determine if token is transferable
     bool private isTokenTransferable;
+    bool private proposalInProgress;
+    uint256 private totalVotes;
+    uint256 private totalYes;
+    mapping(address => bool) private hasVoted;
+    mapping(address => bool) private tokenHolders;
+    address[] private voters;
 
-    // Event emitted when a new token is minted
     event TokenMinted(address indexed user, uint256 indexed tokenId);
-
-    // Event emitted when a token is burned
     event TokenBurned(address indexed user, uint256 indexed tokenId);
-
-    // Modifier to check if token is transferable
-    modifier onlyTransferable() {
-        require(isTokenTransferable, "Token is not transferable");
-        _;
-    }
+    event ProposalCreated(uint256 indexed proposalId, string proposalDescription);
+    event Voted(address indexed voter, bool voteChoice);
+    event ProposalFinalized(bool passed);
 
     constructor() ERC721("Loyalty Token", "LOYALTY") {
         tokenIdCounter = 1;
-        isTokenBurnt[0] = true; // Reserve token ID 0 to represent a burnt token
-        isTokenTransferable = false; // Token is not transferable by default
+        isTokenBurnt[0] = true;
+        isTokenTransferable = false;
+        proposalInProgress = false;
+        totalVotes = 0;
+        totalYes = 0;
     }
 
-    /**
-     * @dev Mint a new token for the user.
-     * Only the contract owner can call this function.
-     */
     function mintToken(address user) external onlyOwner returns (uint256) {
         require(user != address(0), "Invalid user address");
 
         uint256 newTokenId = tokenIdCounter;
         tokenIdCounter++;
 
-        // Mint new token
         _safeMint(user, newTokenId);
 
         emit TokenMinted(user, newTokenId);
@@ -52,10 +45,6 @@ contract LoyaltyApp is ERC721, Ownable {
         return newTokenId;
     }
 
-    /**
-     * @dev Burn a token.
-     * The caller must be the owner of the token or the contract owner.
-     */
     function burnToken(uint256 tokenId) external {
         require(_isApprovedOrOwner(_msgSender(), tokenId), "Caller is not the owner nor approved");
         require(!isTokenBurnt[tokenId], "Token is already burnt");
@@ -66,25 +55,64 @@ contract LoyaltyApp is ERC721, Ownable {
         emit TokenBurned(_msgSender(), tokenId);
     }
 
-    /**
-     * @dev Set whether the token is transferable or not.
-     * Only the contract owner can call this function.
-     */
     function setTokenTransferability(bool transferable) external onlyOwner {
         isTokenTransferable = transferable;
     }
 
-    /**
-     * @dev Check if a token is burnt.
-     */
     function isTokenBurned(uint256 tokenId) external view returns (bool) {
         return isTokenBurnt[tokenId];
     }
 
-    /**
-     * @dev Check if the token is transferable.
-     */
     function getTransferability() external view returns (bool) {
         return isTokenTransferable;
+    }
+
+    function createProposal(string calldata proposalDescription) external {
+        require(tokenHolders[_msgSender()], "Caller does not own tokens");
+        require(!proposalInProgress, "Existing proposal in progress");
+
+        proposalInProgress = true;
+        totalVotes = 0;
+        totalYes = 0;
+
+        emit ProposalCreated(totalVotes, proposalDescription);
+    }
+
+    function vote(bool choice) external {
+        require(tokenHolders[_msgSender()], "Caller does not own tokens");
+        require(proposalInProgress, "No proposal in progress");
+        require(!hasVoted[_msgSender()], "Caller has already voted");
+
+        totalVotes++;
+        if (choice) {
+            totalYes++;
+        }
+
+        hasVoted[_msgSender()] = true;
+        voters.push(_msgSender());
+
+        emit Voted(_msgSender(), choice);
+    }
+
+    function finalizeProposal() external onlyOwner {
+        require(proposalInProgress, "No proposal in progress");
+
+        bool proposalPassed = (totalYes > totalVotes / 2);
+
+        if (proposalPassed) {
+            // Implement proposal action here
+            isTokenTransferable = !isTokenTransferable;
+        }
+
+        // Reset proposal variables
+        proposalInProgress = false;
+        totalVotes = 0;
+        totalYes = 0;
+        for (uint256 i = 0; i < voters.length; i++) {
+            hasVoted[voters[i]] = false;
+        }
+        delete voters;
+
+        emit ProposalFinalized(proposalPassed);
     }
 }
